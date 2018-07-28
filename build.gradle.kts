50446d490c06f2ea80134f47e1440078222f8885
Benedikt Stemmildt <benedikt@stemmildt.com>
2018-07-28 20:09:37 +0200

    feat: :sparkles: Introducing new features
    * Dependency update
    * JobWatcher from lib
    * Better gradle kotlin config

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,34 +14,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 version = "1.0.0-SNAPSHOT"
 
-apply {
-  plugin("kotlin")
-}
+apply { from("$rootDir/gradle/dependencies.gradle.kts") }
 
-configure<JavaPluginConvention> {
-  sourceCompatibility = JavaVersion.VERSION_1_10
-  targetCompatibility = JavaVersion.VERSION_1_10
-}
+val versions = extra["versions"] as Map<*, *>
+val libraries = extra["libraries"] as Map<*, *>
+val testLibraries = extra["testLibraries"] as Map<*, *>
 
-buildscript {
-  project.apply {
-    from("$rootDir/gradle/dependencies.gradle.kts")
-  }
-
-  val gradlePlugins = extra["gradlePlugins"] as Map<*, *>
-
-  repositories {
-    mavenLocal()
-    jcenter()
-    maven { setUrl("https://plugins.gradle.org/m2/") }
-    maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-  }
-  dependencies {
-    classpath(gradlePlugins["kotlin-gradle-plugin"] as String)
-    classpath(gradlePlugins["git-properties"] as String)
-    classpath(gradlePlugins["spring-boot"] as String)
-    classpath(gradlePlugins["versions"] as String)
-  }
+plugins {
+  kotlin("jvm") version "1.2.51"
+  idea
+  id("com.gorylenko.gradle-git-properties") version "1.5.1"
+  id("org.springframework.boot") version "2.0.3.RELEASE"
+  id("com.github.ben-manes.versions") version "0.20.0"
 }
 
 repositories {
@@ -50,48 +34,44 @@ repositories {
   maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
 }
 
-val versions = extra["versions"] as Map<*, *>
-val libraries = extra["libraries"] as Map<*, *>
-val testLibraries = extra["testLibraries"] as Map<*, *>
-
 ext["thymeleaf.version"] = versions["thymeleaf"]
 ext["thymeleaf-layout-dialect.version"] = versions["thymeleaf-layout-dialect"]
 
 dependencies {
-  "compileOnly"(libraries["spring-context-indexer"] as String)
+  compileOnly(libraries["spring-context-indexer"] as String)
 
-  "compile"(libraries["spring-boot-starter-data-mongodb-reactive"] as String)
-  "compile"(libraries["spring-boot-starter-data-rest"] as String)
-  "compile"(libraries["spring-boot-starter-webflux"] as String)
-  "compile"(libraries["spring-boot-starter-breuninger-core"] as String)
-  "compile"(libraries["spring-boot-starter-breuninger-jobs"] as String)
-  "compile"(libraries["spring-boot-starter-breuninger-mongo"] as String)
-  "compile"(libraries["spring-boot-starter-breuninger-togglz"] as String)
+  compile(libraries["spring-boot-starter-data-mongodb-reactive"] as String)
+  compile(libraries["spring-boot-starter-data-rest"] as String)
+  compile(libraries["spring-boot-starter-webflux"] as String)
+  compile(libraries["spring-boot-starter-breuninger-core"] as String)
+  compile(libraries["spring-boot-starter-breuninger-jobs"] as String)
+  compile(libraries["spring-boot-starter-breuninger-mongo"] as String)
+  compile(libraries["spring-boot-starter-breuninger-togglz"] as String)
 
-  "compile"(libraries["kotlin-stdlib"] as String)
-  "compile"(libraries["kotlin-stdlib-jre8"] as String)
-  "compile"(libraries["kotlin-stdlib-jre7"] as String)
-  "compile"(libraries["kotlin-reflect"] as String)
-  "compile"(libraries["jackson-module-kotlin"] as String)
+  compile(libraries["kotlin-stdlib"] as String)
+  compile(libraries["kotlin-stdlib-jre8"] as String)
+  compile(libraries["kotlin-stdlib-jre7"] as String)
+  compile(libraries["kotlin-reflect"] as String)
+  compile(libraries["jackson-module-kotlin"] as String)
 
-  "compile"(libraries["spring-boot-devtools"] as String)
+  compile(libraries["spring-boot-devtools"] as String)
 
-  "testCompile"(testLibraries["embed-mongo"] as String)
-  "testCompile"(testLibraries["spring-boot-starter-breuninger-testsupport"] as String)
-  "testCompile"(testLibraries["spring-boot-starter-test"] as String)
+  testCompile(testLibraries["embed-mongo"] as String)
+  testCompile(testLibraries["spring-boot-starter-breuninger-testsupport"] as String)
+  testCompile(testLibraries["spring-boot-starter-test"] as String)
 
-  "testCompile"(testLibraries["reactor-test"] as String)
-  "testCompile"(testLibraries["mockito-kotlin"] as String)
-  "testCompile"(testLibraries["hamkrest"] as String)
+  testCompile(testLibraries["reactor-test"] as String)
+  testCompile(testLibraries["mockito-kotlin"] as String)
+  testCompile(testLibraries["hamkrest"] as String)
 }
 
 apply {
-  plugin("idea")
-  plugin("com.gorylenko.gradle-git-properties")
-  plugin("org.springframework.boot")
-  plugin("com.github.ben-manes.versions")
-
   from("$rootDir/gradle/test.gradle.kts")
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
 }
 
 tasks {
@@ -101,7 +81,7 @@ tasks {
     }
   }
 
-  withType<BootRun> {
+  getByName<BootRun>("bootRun") {
     systemProperties = System.getProperties().mapKeys { entry -> entry.key.toString() }.toMap()
   }
 }
